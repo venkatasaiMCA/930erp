b10a72f6d85a8a87448b4856bbf5709c5efac010
VENKATASAI BADUGU <93070971+venkatasaiMCA@users.noreply.github.com>
2024-10-17 16:16:55 -0400

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -1,14 +1,5 @@
-public class JavaExample {
-  public static void main(String[] args) {
-    // two integer variables with values
-    // and a variable "sum" to store the result
-    int num1 = 5, num2 = 15,sum;
 
-    //calculating the sum of num1 and num2 and
-    //storing the result in the variable sum
-    sum = num1+num2;
-
-    //printing the result
-    System.out.println("Sum of "+num1+" and "+num2+" is: "+sum);
-  }
-}
+int x = 5;
+int y = 6;
+int sum = x + y;
+System.out.println(sum);
