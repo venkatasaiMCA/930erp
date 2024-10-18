d701a086845d179c4bdad2b88adb2bed2957b64c
VENKATASAI BADUGU <93070971+venkatasaiMCA@users.noreply.github.com>
2024-10-17 23:13:18 -0400

    Add files via upload

---
+++ StudentInformation.java
@@ -0,0 +1,22 @@
+public class StudentInformation {
+
+    public static void main(String[] args) {
+        printStudentInfo();
+    }
+
+    private static void printStudentInfo() {
+        String collegeName = "PYTHONLIFE";
+        String studentName = "KRISHNA";
+        int rollNumber = 1596;
+
+        System.out.println("College Name: " + collegeName);
+        System.out.println("Student Name: " + studentName);
+        System.out.println("Roll Number: " + rollNumber);
+
+        System.out.println("\nWelcome, " + studentName + "!");
+        System.out.println("We hope you have a fantastic journey at " + collegeName + ".");  
+        System.out.println("Learn Here and Lead anywhere!!!!!!!!!");
+        System.out.println("!!!!!!!THANK YOU!!!!!!!!!");
+    }
+}
+
